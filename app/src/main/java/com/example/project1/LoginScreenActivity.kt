4198690907304ecaed7369489ef7335daede2d51
[example] <[bansalsneha991]>
2024-10-20 09:35:23 +0530

    linked create new account

--- app/src/main/java/com/example/project1/LoginScreenActivity.kt
+++ app/src/main/java/com/example/project1/LoginScreenActivity.kt
@@ -1,6 +1,9 @@
 package com.example.project1
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,11 @@ class LoginScreenActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val newAccount = findViewById<TextView>(R.id.new_account)
+        newAccount.setOnClickListener {
+            val intent = Intent(this,RegisterPageActivity::class.java)
+            startActivity(intent)
         }
     }
 }
